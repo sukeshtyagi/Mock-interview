de37070aaf76147f4e63ea46c8fc9ff1e14add3f
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-04-16 09:18:50 +0530

    Promise resolved, rejected, pending

--- zcodej.js
+++ zcodej.js
@@ -18,10 +18,33 @@ function foo() {
 foo();
 */
 
-
+/*
 for (let index =1; index <3; index++) {
   setTimeout(() => {
     console.log(index)
   }, 1000);
   
 }
+*/
+
+async function foo() {
+  return "hello world";
+}
+const result = foo();
+result.then((res) => console.log(res));
+
+//resolve a promise by retrunign a vale and
+//reject it by throwing an error
+//pending by returnina  a new promsie
+
+async function foo() {
+  throw new Error("hello");
+}
+const res = foo();
+console.log(res);
+
+async function foo() {
+  return new Promise(() => {});
+}
+const res1 = foo();
+console.log(res1)

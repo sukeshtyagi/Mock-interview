ca8d94c72106efc3bb43e10f9bd954cbb800ccf4
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-04-16 06:34:54 +0530

    Practical Qn

--- zcodej.js
+++ zcodej.js
@@ -1,25 +1,7 @@
-const str = "hi i m coder";
+function foo() {
+  var x=10
+}
 
-const arry = str.split(" ");
+foo()
 
-//console.log(arry)
-
-const capitalArray = arry.map((word) => {
-  return word[0].toUpperCase();
-});
-
-console.log(capitalArray);
-
-const sub = arry.map((word) => {
-  const st = word.substring(1);
-  return st;
-});
-console.log(sub);
-
-let word1 = capitalArray[0] + sub[0];
-let word2 = capitalArray[1] + sub[1];
-let word3 = capitalArray[2] + sub[2];
-let word4 = capitalArray[3] + sub[3];
-
-const final = [word1, word2, word3, word4].join(" ");
-console.log(final)
+console.log(x)

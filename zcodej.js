7d30ecfccea1ea276fb55e999c99fdff743047de
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-04-16 07:06:19 +0530

    conflicting name fro var and function

--- zcodej.js
+++ zcodej.js
@@ -1,8 +1,17 @@
-function foo() {
+/*function foo() {
   console.log((x))
    x=10
 }
 
 foo()
 
-console.log(typeof(x))
+console.log(typeof(x))*/
+
+foo();
+var foo = 20;
+
+function foo() {
+  console.log("calling foo");
+}
+
+foo();

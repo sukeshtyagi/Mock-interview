e431940e0ff6839ebd7a074276df98138256f345
sukesh <sukeshtyagi05@gmail.com>
2024-05-07 12:27:25 +0530

    str rev()

--- zcode2.js
+++ zcode2.js
@@ -1,11 +1,14 @@
 const str = "hello world";
+
+const ary11 = str.split("").reverse().join("");
+console.log(ary11);
+
 const ary = str.split(" ");
 console.log(ary);
 
 const word1 = ary[0];
 const word2 = ary[1];
-const ary11 = str.split("").reverse().join("");
-console.log(ary11);
+
 const ary1 = word1.split("").reverse().join("");
 const ary2 = word2.split("").reverse().join("");
 
